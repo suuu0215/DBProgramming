21c7560ac21c341bb3ab2631ed354dc90565c0da
수안 최 <chtndks@naver.com>
2020-10-10 00:01:28 +0900

    DBnewmerge

--- DB1/Form1.cs
+++ DB1/Form1.cs
@@ -19,7 +19,7 @@ namespace DB1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "Hi~~~";
+            textBox1.Text = "Bye~~~";
             MessageBox.Show("i'm good!");
         }
     }
